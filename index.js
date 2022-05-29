b9965f7a16780cb551d05823a62acb06e0a189b2
I Gusti Agung Sugihartha <igustiagungsugihartha@gmail.com>
2022-05-30 00:20:51 +0800

    feat: Perbaikan Array Favorite Restaurant User Monica

--- index.js
+++ index.js
@@ -2,43 +2,40 @@
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
 
-let firstUserFavColor = new Set(["Yellow", "Pink", "White", "Purple"]);
+let favColor = ["Yellow", "Pink", "White", "Purple", "Blue", "Black", "Grey" ];
+let favRestaurant = ["Pancake", "Padang", "Tteok", "Tempura", "Bento", "Sushi", "Katsu", "Geprek", "Eggy"];
 let firstUserEducation = new Set([
     { name: "SD 01", city: "Jakarta", graduate: 2016 },
     { name: "SMP 02", city: "Jakarta", graduate: 2019 },
     { name: "SMA 03", city: "Tanggerang", graduate: 2022 }
 ]);
-let firstUserFavRestaurant = new Set(["Bento", "Pancake", "Eggy", "Tempura", "Padang", "Tteok"]);
-
-const firstUser = {
-    name: "Monica",
-    gender: "Female",
-    age: 17,
-    email: "monica@dingdong.com",
-    favoriteColor: [...firstUserFavColor],
-    isHavePet: "Yes",
-    education: [...firstUserEducation],
-    favoriteRestaurant: [...firstUserFavRestaurant]
-};
-
-let secondUserFavColor = new Set(["Blue", "Black", "Grey"]);
 let secondUserEducation = new Set([
     { name: "SD 02", city: "Jakarta", graduate: 2010 },
     { name: "SMP 03", city: "Bogor", graduate: 2013 },
     { name: "SMA 01", city: "Surabaya", graduate: 2016 },
     { name: "Universitas Maju", city: "Tanggerang", graduate: 2020 }
 ]);
-let secondUserFavRestaurant = new Set(["Tempura", "Bento", "Sushi", "Pancake", "Padang", "Katsu", "Geprek", "Eggy"]);
 
+const firstUser = {
+    name: "Monica",
+    gender: "Female",
+    age: 17,
+    email: "monica@dingdong.com",
+    favoriteColor: [favColor[0], favColor[1], favColor[2], favColor[3]],
+    isHavePet: "Yes",
+    education: [...firstUserEducation],
+    favoriteRestaurant: [favRestaurant[4], favRestaurant[5], favRestaurant[0], favRestaurant[8], favRestaurant[3], favRestaurant[1], favRestaurant[2]]
+};
+//["Pancake", "Padang", "Tteok", "Tempura", "Bento", "Sushi", "Katsu", "Geprek", "Eggy"]
 const secondUser = {
     name: "Wendy",
     gender: "Male",
     age: 23,
     email: "wendy@dingdong.com",
-    favoriteColor: [...secondUserFavColor],
+    favoriteColor: [favColor[4], favColor[5], favColor[6]],
     isHavePet: "No",
     education: [...secondUserEducation],
-    favoriteRestaurant: [...secondUserFavRestaurant]
+    favoriteRestaurant: [favRestaurant[3], favRestaurant[4], favRestaurant[5], favRestaurant[0], favRestaurant[1], favRestaurant[6], favRestaurant[7], favRestaurant[8]]
 };
 
 
